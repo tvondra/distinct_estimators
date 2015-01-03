50a5a3814df012204aeca0ab8f3aa88a030d5b01
Tomas Vondra <tv@fuzzy.cz>
2015-01-04 00:49:01 +0100

    fix version mixup (SQL script versioned incorrectly), bump to 1.3.4

--- bitmap/test/sql/base.sql
+++ bitmap/test/sql/base.sql
@@ -3,7 +3,7 @@ BEGIN;
 
 -- disable the notices for the create script (shell types etc.)
 SET client_min_messages = 'WARNING';
-\i sql/bitmap_counter--1.3.3.sql
+\i sql/bitmap_counter--1.3.4.sql
 SET client_min_messages = 'NOTICE';
 
 \set ECHO all
