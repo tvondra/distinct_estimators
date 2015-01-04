7d437c4818cba17d89138ba2cb7a5f97a6a8c963
Tomas Vondra <tv@fuzzy.cz>
2015-01-04 14:34:41 +0100

    using explicit module path instead of MODULE_PATHNAME

--- superloglog/sql/superloglog_counter--1.2.0.sql
+++ superloglog/sql/superloglog_counter--1.2.0.sql
@@ -5,62 +5,62 @@ CREATE TYPE superloglog_estimator;
 
 -- get estimator size for the requested error rate
 CREATE FUNCTION superloglog_size(error_rate real) RETURNS int
-     AS 'MODULE_PATHNAME', 'superloglog_size'
+     AS '$libdir/superloglog_counter', 'superloglog_size'
      LANGUAGE C;
 
 -- creates a new LogLog estimator with a given a desired error rate limit
 CREATE FUNCTION superloglog_init(error_rate real) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_init'
+     AS '$libdir/superloglog_counter', 'superloglog_init'
      LANGUAGE C;
 
 -- merges the second estimator into the first one
 CREATE FUNCTION superloglog_merge(estimator1 superloglog_estimator, estimator2 superloglog_estimator) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_merge_simple'
+     AS '$libdir/superloglog_counter', 'superloglog_merge_simple'
      LANGUAGE C;
 
 -- merges the second estimator into the first one
 CREATE FUNCTION superloglog_merge_agg(estimator1 superloglog_estimator, estimator2 superloglog_estimator) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_merge_agg'
+     AS '$libdir/superloglog_counter', 'superloglog_merge_agg'
      LANGUAGE C;
 
 -- add an item to the estimator
 CREATE FUNCTION superloglog_add_item(counter superloglog_estimator, item anyelement) RETURNS void
-     AS 'MODULE_PATHNAME', 'superloglog_add_item'
+     AS '$libdir/superloglog_counter', 'superloglog_add_item'
      LANGUAGE C;
 
 -- get current estimate of the distinct values (as a real number)
 CREATE FUNCTION superloglog_get_estimate(counter superloglog_estimator) RETURNS real
-     AS 'MODULE_PATHNAME', 'superloglog_get_estimate'
+     AS '$libdir/superloglog_counter', 'superloglog_get_estimate'
      LANGUAGE C STRICT;
 
 -- reset the estimator (start counting from the beginning)
 CREATE FUNCTION superloglog_reset(counter superloglog_estimator) RETURNS void
-     AS 'MODULE_PATHNAME', 'superloglog_reset'
+     AS '$libdir/superloglog_counter', 'superloglog_reset'
      LANGUAGE C STRICT;
 
 -- length of the estimator (about the same as superloglog_size with existing estimator)
 CREATE FUNCTION length(counter superloglog_estimator) RETURNS int
-     AS 'MODULE_PATHNAME', 'superloglog_length'
+     AS '$libdir/superloglog_counter', 'superloglog_length'
      LANGUAGE C STRICT;
 
 /* functions for aggregate functions */
 
 CREATE FUNCTION superloglog_add_item_agg(counter superloglog_estimator, item anyelement, error_rate real) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_add_item_agg'
+     AS '$libdir/superloglog_counter', 'superloglog_add_item_agg'
      LANGUAGE C;
 
 CREATE FUNCTION superloglog_add_item_agg2(counter superloglog_estimator, item anyelement) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_add_item_agg2'
+     AS '$libdir/superloglog_counter', 'superloglog_add_item_agg2'
      LANGUAGE C;
 
 /* input/output functions */
 
 CREATE FUNCTION superloglog_in(value cstring) RETURNS superloglog_estimator
-     AS 'MODULE_PATHNAME', 'superloglog_in'
+     AS '$libdir/superloglog_counter', 'superloglog_in'
      LANGUAGE C IMMUTABLE STRICT;
 
 CREATE FUNCTION superloglog_out(counter superloglog_estimator) RETURNS cstring
-     AS 'MODULE_PATHNAME', 'superloglog_out'
+     AS '$libdir/superloglog_counter', 'superloglog_out'
      LANGUAGE C IMMUTABLE STRICT;
 
 -- actual LogLog counter data type
